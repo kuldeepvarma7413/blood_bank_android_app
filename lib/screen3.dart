191802df1d19fbf77f511e66ad6e545ade7a9035
Kul Deep Varma <kuldeepvarma7413@gmail.com>
2023-07-01 09:42:30 +0530

    redesigned first 4 screens and added otp 1st screen plus font families

--- lib/screen3.dart
+++ lib/screen3.dart
@@ -25,20 +25,24 @@ class App extends StatelessWidget {
               // crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  height: 400,
+                  height: 450,
+                  margin: EdgeInsets.all(0),
                   alignment: Alignment.bottomCenter,
-                  child: Image.asset('images/puzzle.png'),
+                  child: Image.asset(
+                    'images/puzzle.png',
+                    fit: BoxFit.fitWidth,
+                    height: 278,
+                  ),
                 ),
                 Container(
-                  alignment: Alignment.center,
-                  width: 300,
                   margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
                   child: Text(
                     "Your blood type should be compatible with the receiver’s type.",
                     style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                    ),
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontFamily: 'PoorStory'),
+                    textAlign: TextAlign.center,
                   ),
                 )
               ],
